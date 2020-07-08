cf2addec4c30f12736d79b97d32725dc0579714f
ser-yk <56737901+ser-yk@users.noreply.github.com>
2020-07-08 12:52:19 +0300

    Update conftest.py

--- task_2/conftest.py
+++ task_2/conftest.py
@@ -1,13 +1,16 @@
 import pytest
 from selenium import webdriver
 import os
+from sys import platform
 
 @pytest.yield_fixture(scope='session')
 def browser():
     browser = webdriver.Chrome()
     yield browser
     browser.quit()
-    os.system("taskkill /im chromedriver.exe")
+    if platform == "win32":
+        os.system("taskkill /im chromedriver.exe")
+
 
 
 
